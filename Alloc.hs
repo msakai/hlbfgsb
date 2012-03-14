27caab7e46afafd31b68ec957cf3467e13c8d4b4
ivan <ivan@hlbfgsb.ksp.sk>
2012-03-14 22:19:14 +0900

    allocator protector
    Ignore-this: 164f3172a709e1a77b8666537ccd8471
    
    darcs-hash:e46dde5e801acbc28a24c09ca4a17f8892c26560

---
+++ Alloc.hs
@@ -0,0 +1,47 @@
+module Alloc
+
+where
+
+import Control.Monad
+import qualified Foreign as F
+import Foreign (Word8, peekArray, pokeArray)
+import Foreign.Ptr
+import Foreign.Storable
+
+alloca :: Storable a => (Ptr a -> IO b) -> IO b
+alloca f = allocaArray 1 f
+
+with :: Storable a => a -> (Ptr a -> IO b) -> IO b
+with x = withArray [x]
+
+c :: Int
+c = 470
+
+allocaArray :: Storable a => Int -> (Ptr a -> IO b) -> IO b
+allocaArray n f = F.allocaArray (n+2*c) $ \ptr -> do
+    let s = sizeOf (ptrType ptr)
+	band = replicate (c * s) (47 :: Word8)
+	begin = castPtr ptr
+	data_ = ptr `plusPtr` (c * s) `asTypeOf` ptr
+	end = castPtr $ data_ `plusPtr` (n * s) in do
+    putStrLn $ "Alloc " ++ show n ++ " x " ++ show s
+    pokeArray begin band
+    pokeArray end band
+    res <- f data_
+    b <- peekArray (c * s) begin
+    e <- peekArray (c * s) end
+    when (b /= band || e /= band) $ do
+	putStrLn "\n\n\nDetected invalid memory access!!!\n"
+	print b
+	print e
+	putStrLn "\n\n\n"
+    putStrLn $ "Free " ++ show n ++ " x " ++ show s
+    return res
+  where
+    ptrType :: Ptr a -> a
+    ptrType _ = undefined
+
+withArray :: Storable a => [a] -> (Ptr a -> IO b) -> IO b
+withArray xs f = allocaArray (length xs) $ \ptr -> do
+    pokeArray ptr xs
+    f ptr
